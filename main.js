e21f7f47a0b31e9b63f4ca7fd1186a2e719532b0
KapQ <kquarles18@gmail.com>
2017-06-26 16:33:48 -0400

    flexboxmod

--- main.js
+++ main.js
@@ -2,6 +2,18 @@
 // 2. Loop over the data
 // 3. Display it in the `.customers` element
 (function () {
+  fetch("https://randomuser.me/api/?results=12")
+  .then(function(response){
+    return response.json();
+  })
+  .then(function(json){
+    console.log(json.results[0])
+
+
+
+
+
+  });
 
   'use strict';
 
